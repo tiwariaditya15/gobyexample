550308b266f1403f08c4a35d205f7f06b2f3d289
Aditya Tiwari <tiwariaditya15@hotmail.com>
2024-02-25 12:09:49 +0530

    exercise: non-blocking channels

---
+++ gobyexample/31-non-blocking-channel-ops.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	// Non-blocking receive
+	select {
+	case msg := <-messages:
+		fmt.Println(msg)
+	default:
+		fmt.Println("No messages received!")
+	}
+
+	msg := "hello there!"
+	// Non-blocking send
+	select {
+	case messages<- msg:
+		fmt.Println("Sent to messages!")
+	default:
+		fmt.Println("Message channel not ready to accept.")
+	}
+
+	// multi-way-non-blocking
+	select {
+	case msg := <-messages:
+		fmt.Println("Received on messages channel:", msg)
+	case sig := <-signals:
+		fmt.Println("Received on signals channel:", sig)
+	default:
+		fmt.Println("Default way.")
+	}
+}
