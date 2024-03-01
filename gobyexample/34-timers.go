0f5fd14d490c86b52bc55a989c972fffead63215
Aditya Tiwari <tiwariaditya15@hotmail.com>
2024-03-01 23:58:54 +0530

    34

--- gobyexample/34-timers.go
+++ gobyexample/34-timers.go
@@ -10,11 +10,13 @@ func main() {
 	<-timer1.C
 	fmt.Println("timer1 fired.")
 
-	timer2 := time.NewTimer(time.Second)
+	timer2 := time.NewTimer(5 * time.Second)
 	go func ()  {
 		<-timer2.C
 		fmt.Println("timer2 fired.")
 	}()
-
+	// Since timer2 needs 5 seconds the goroutine started and eof reached where we stopped the timer2
+	timer2.Stop()
 	time.Sleep(2 * time.Second)
+	fmt.Println("End of file")
 }
