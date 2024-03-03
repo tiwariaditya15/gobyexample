10f8f8ce2d3d667ef979fde466e2d77a3161643f
Aditya Tiwari <tiwariaditya15@hotmail.com>
2024-03-03 20:29:30 +0530

    rate-limiter

---
+++ gobyexample/38-rate-limiter.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {
+	requests := make(chan int, 5)
+	for i := 1;i <= 5; i++ {
+		requests<- i
+	}
+	close(requests)
+
+	limiter := time.Tick(200 * time.Millisecond)
+
+	for request := range requests {
+		<-limiter
+		fmt.Println("Serving request:", request)
+	}
+}
